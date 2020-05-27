ca348a2324661a7138fe471f4b065f5a36ae6582
Josh <gibbons.josh@outlook.com>
2020-05-27 12:49:26 -0600

    mini proj

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+require('dotenv').config()
+
 const express = require('express'),
-      port = 3333,
+      massive = require('massive'),
+      ctrl = require('./controller'),
+      {SERVER_PORT, CONNECTION_STRING} = process.env,
+      port = SERVER_PORT,
       app = express();
 
+
+
 app.use(express.json());
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {rejectUnauthorized: false}
+}).then(db => {
+  app.set('db', db)
+  console.log('db connected')
+})
+
+app.post('/api/movies', ctrl.addMovie)
+app.get('/api/movies', ctrl.getMovies)
+app.put('/api/movies/:id', ctrl.updateMovie)
+app.delete('/api/movies/:id', ctrl.deleteMovie)
 
 app.listen(port, () => console.log(`Server running on ${port}`));
